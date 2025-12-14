77f649df84ff3a037f968c5ce4d8d9b9961cb5c0
gylixa <vpopoviceva33@gmail.com>
2025-12-14 21:44:26 +0300

    добавление страницы с меню

--- coffee/coffee/urls.py
+++ coffee/coffee/urls.py
@@ -21,4 +21,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'), 
+    path('menu/', views.menu, name='menu'),
 ]
--- coffee/main/views.py
+++ coffee/main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 def home(request):
     return render(request, 'home.html')
+
+def menu(request):
+    items = MenuItem.objects.all().order_by('category', 'name')
+    # Группируем по категориям
+    menu_dict = {}
+    for item in items:
+        cat_display = dict(MenuItem.CATEGORY_CHOICES)[item.category]
+        menu_dict.setdefault(cat_display, []).append(item)
+    return render(request, 'menu.html', {'menu_items': menu_dict})
